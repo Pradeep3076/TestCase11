eb2697744433d053eb6f373fc1ccb05fc1ff59c4
Pradeep3076 <pradeepashok.1994@yahoo.com>
2019-08-23 12:29:29 +0530

    message

---
+++ src/org/tcs/test/newone.java
@@ -0,0 +1,12 @@
+package org.tcs.test;
+
+public class newone {
+	public  void empId() {
+		System.out.println("1234567");
+	}
+public static void main(String[] args) {
+	
+	newone n = new newone();
+	n.empId();
+}
+}
